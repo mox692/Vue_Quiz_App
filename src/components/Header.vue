2d3554d5ad4769bbe9f065a4ed0a542ba020440b
mox692 <55653825+mox692@users.noreply.github.com>
2020-07-21 20:36:55 +0900

    create top page,ending page

---
+++ src/components/Header.vue
@@ -0,0 +1,28 @@
+<template>
+<div>
+    <p>{{ this.correctAnswers }}/{{ this.submittedQuestion }}</p>
+</div>
+
+</template>
+<script>
+export default{
+
+  props: {
+      correctAnswers: Number,
+      submittedQuestion: Number,
+  },
+  data(){
+    return{
+    }
+  }
+
+}
+
+</script>
+
+<style scoped>
+p {
+  /* font-size: 30px; */
+}
+
+</style>
---
+++ src/components/Titlepage.vue
@@ -0,0 +1,17 @@
+<template>
+    <div class="title-wrapper">
+        <img alt="Vue logo" src="../assets/logo.png">
+        <h1>Welcome To CS Quiz App!!!</h1>
+         <p>hi! Let's try your CS knowledge here:)</p>
+    </div>
+</template>
+
+<script>
+</script>
+
+<style scoped>
+.title-wrapper{
+    padding-bottom: 100px;
+    /* background-color: red; */
+}
+</style>
