45dd762b499cc5826eadec2cc9e95a27a7144d67
mox692 <55653825+mox692@users.noreply.github.com>
2020-07-21 11:25:51 +0900

    create quiz disp

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,26 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-
   <!-- 
     ・カスタム属性を定義している。
     ・子にデータを渡している。
-    ・question,indexは、dataとして参照されている
+    ・左辺のquestion,indexなどは、data内の変数として参照されている
    -->
     <Questionbox 
+      v-if="questions.length"
       :currentQuestion="questions[index]"
+      :next="next"
     />
   </div>
 </template>
-
 <script>
-
+// ・App.vueはimportも含む。
+// ・上のテンプレートで使用するVueオブジェクトはexport内で定義する。
 import Questionbox from './components/Questionbox.vue'
-
 export default {
   name: 'App',
   components: {
-    Questionbox
+    Questionbox //外部テンプレートを読み込む場合。
   },
   data() {
     return {
@@ -38,10 +38,14 @@ export default {
       ]
     }
   },
+  methods: {
+    next: function(){
+          this.index++;
+      }
+  },
   // loadした時にクイズを読み込む
   mounted: function(){
     let self = this
-    console.log(this.questions);
     fetch('https://opentdb.com/api.php?amount=10&category=18')
     .then(function(response) {
       return response.json();
